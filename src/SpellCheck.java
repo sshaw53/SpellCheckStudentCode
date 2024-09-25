ca7ad6759968f8278e264d09d1d80d916e11eb8a
sierrashaw <sierra.shaw@menloschool.org>
2024-09-25 14:43:52 -0700

    Oops it was 30 mins not 1.5 hours.

--- src/SpellCheck.java
+++ src/SpellCheck.java
@@ -15,10 +15,51 @@ public class SpellCheck {
      *
      * @param text The list of all words in the text.
      * @param dictionary The list of all accepted words.
-     * @return String[] of all mispelled words in the order they appear in text. No duplicates.
+     * @return String[] of all misspelled words in the order they appear in text. No duplicates.
      */
     public String[] checkWords(String[] text, String[] dictionary) {
+        // Set up the map of words
+        int[] clusters = new int[dictionary.length + 1];
+        int current_idx = 0;
+        int previous_idx = 0;
+        // Traverse through every word in the dictionary
+        for (int i = 1; i < dictionary.length; i++) {
+            // If we have hit a root, we need to start a new cluster
+            current_idx = i;
+            if (clusters[previous_idx] == 0) {
+                previous_idx = current_idx;
+                break;
+            }
 
+            // Traverse up the tree until you find the prefix matching the last checked word
+            else {
+                while (dictionary[previous_idx] != dictionary[current_idx].substring(0, dictionary[previous_idx].length())) {
+                    // If you hit a root and there's still nothing, create a new cluster
+                    if (clusters[previous_idx] == 0) {
+                        clusters[current_idx] = 0;
+                        previous_idx = current_idx;
+                        break;
+                    }
+                    else {
+                        previous_idx = clusters[previous_idx];
+                    }
+                }
+                // If the new word starts (prefixes) with the last checked word
+                // Set previous word's index as the root of the new word
+                clusters[current_idx] = previous_idx;
+                previous_idx = current_idx;
+            }
+        }
+
+        // For each word in the string
+            // For each root
+                // Check to see if the root string is a part of the word
+                    // When it is, it will go down that tree to find the word
+                    // If the word is present, it will go to the next word in the string
+                    // If not present, it will check to see if the word has already been added to the return array
+                    // Add if not already added
         return null;
     }
+
+    // Maybe think about cleaning the string .. will it make it more efficient?
 }

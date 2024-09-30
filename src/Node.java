239d647eef1dc28e5ab6672aba93b9a1ee683eac
sierrashaw <sierra.shaw@menloschool.org>
2024-09-29 19:32:52 -0700

    Learning how to implement a trie.

---
+++ src/Node.java
@@ -0,0 +1,19 @@
+public class Node {
+    private boolean isWord;
+    private Node[] next;
+
+    // Constructor
+
+    // Other Functions
+    public boolean isWord() {
+        return isWord;
+    }
+
+    public void setWord() {
+        isWord = true;
+    }
+
+    public Node[] getNext() {
+        return next;
+    }
+}
---
+++ src/Trie.java
@@ -0,0 +1,22 @@
+public class Trie {
+    private final static int radix = 27;
+    private Node root = new Node();
+
+    private static class Node {
+        private Object val;
+        private Node[] next = new Node[radix];
+    }
+
+    public void insert(String s) {
+        //
+    }
+
+    public boolean lookup(String s) {
+        //
+    }
+
+    public void printTrie() {
+        //
+    }
+
+}
